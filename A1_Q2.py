3dcb24810fa565d06a1abff050e37c23a483eea5
Suryanshi2003 <122363359+Suryanshi2003@users.noreply.github.com>
2023-01-16 18:34:56 +0530

    Submitting Q2 of Assignment 1

---
+++ A1_Q2.py
@@ -0,0 +1,11 @@
+marks=int(input())
+if (marks>90):
+    print('Excellent')
+elif (90>=marks>80):
+    print('Good')
+elif (80>=marks>70):
+    print('Fair')
+elif (70>=marks>60):
+    print('Meets expectations')
+elif (marks<=60):
+    print('Below par')
